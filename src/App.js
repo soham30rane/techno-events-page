2fb25bae2891fdcde96cfe069d3c29eb0f9c7af6
Abhay Upadhyay <urabhay10@gmail.com>
2023-11-03 22:12:09 +0530

    temporary eventinfo page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import EventInfo from './components/Events/EventInfo';
 import Events from './components/Events/events';
 
 function App() {
   return (
     <div className="App">
-      <Events />
+      <Events title='Refaktor' eventDesc="We present you our National Level Hackathon, REFAKTOR, a platform to put your imagination against th..." imgname={'refaktor.png'}/>
     </div>
   );
 }
