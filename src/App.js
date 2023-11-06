34a41b3f3f4799651919dbcb60b5e86b817e0356
Abhay Upadhyay <urabhay10@gmail.com>
2023-11-06 14:57:29 +0530

    few final changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import EventInfo from './components/Events/EventInfo';
 import Events from './components/Events/events';
 
 function App() {
   return (
     <div className="App">
-      <Events title='Refaktor' eventDesc="We present you our National Level Hackathon, REFAKTOR, a platform to put your imagination against th..." imgname={'refaktor.png'}/>
+      <Events />
     </div>
   );
 }
